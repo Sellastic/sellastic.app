ef464d324ae26b0253ef213d29c30dbbb3db9069
Ferhat Mousavi <ferhat@mousavi.net>
2022-11-29 12:17:16 +0300

    Update cashier model

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from pos.manager import Application
 
+__version__ = "1.0.0b1"
 
 app = Application()
 app.run()
--- data_layer/models/cashier.py
+++ data_layer/models/cashier.py
@@ -1,19 +1,23 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, BigInteger, String, Boolean, DateTime
+from sqlalchemy.sql import func
+
 from data_layer.models.model import Model
 
 
 class Cashier(Model):
     __tablename__ = "cashier"
 
-    id = Column(Integer, primary_key=True)
-    user_name = Column(String(50), nullable=False,)
-    name = Column(String(50), nullable=False,)
-    last_name = Column(String(50), nullable=False,)
-    password = Column(String(50), nullable=False,)
+    id = Column(BigInteger, primary_key=True, autoincrement="auto")
+    user_name = Column(String(50), unique=True, nullable=False)
+    name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    password = Column(String(50), nullable=False)
     identity_number = Column(String(24))
     description = Column(String(100))
-    is_administrator = Boolean(False)
-    is_active = Boolean(False)
+    is_administrator = Column(Boolean(False))
+    is_active = Column(Boolean(False))
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now())
 
     def __repr__(self):
         return f"<User(name='{self.name}', last_name='{self.last_name}', user_name='{self.user_name}')>"
