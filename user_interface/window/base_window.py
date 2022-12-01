6be9599b40f3830c39a1b0eed176113c810389db
Ferhat Mousavi <ferhat@mousavi.net>
2022-12-01 17:57:48 +0300

    Set window background and foreground color. set fixed size window.

--- user_interface/window/base_window.py
+++ user_interface/window/base_window.py
@@ -10,8 +10,16 @@ class BaseWindow(QMainWindow):
         self.setWindowFlags(Qt.FramelessWindowHint)
 
     def draw_window(self, settings: dict, design: list):
+        p = self.palette()
+        p.setColor(self.backgroundRole(), settings['background_color'])
+        p.setColor(self.foregroundRole(), settings['foreground_color'])
+        self.setPalette(p)
+
+        self.setStyleSheet(f"background-color: {settings['background_color']:02X};")
         self.setWindowTitle(settings["name"])
         self.setGeometry(0, 0, settings["width"], settings["height"])
+        self.setFixedSize(settings["width"], settings["height"])
+
         if settings["toolbar"]:
             self._create_toolbar()
         if settings["statusbar"]:
