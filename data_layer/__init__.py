9f802d9c60c672a5b5e8b929c57c1138ca05ef82
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2022-12-23 21:45:21 +0300

    Change model definitions path

--- data_layer/__init__.py
+++ data_layer/__init__.py
@@ -1,8 +1,8 @@
 from data_layer.model import metadata
 from data_layer.engine import Engine
 
-from data_layer.model.cashier import Cashier
-from data_layer.model.vat import Vat
+from data_layer.model import Cashier
+from data_layer.model import Vat
 
 
 def init_db():
--- data_layer/model/__init__.py
+++ data_layer/model/__init__.py
@@ -1,1 +1,5 @@
 from data_layer.model.crud_model import CRUD, Model, metadata
+
+from data_layer.model.definition.cashier import Cashier
+from data_layer.model.definition.vat import Vat
+
