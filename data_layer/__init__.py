5c716838560b0c31f64df4135b9264ff4904e423
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2022-12-24 02:34:12 +0300

    StockMainGroup data model added.

--- data_layer/__init__.py
+++ data_layer/__init__.py
@@ -2,8 +2,9 @@ from data_layer.model import metadata
 from data_layer.engine import Engine
 
 from data_layer.model import Cashier
-from data_layer.model import StockUnit
 from data_layer.model import Vat
+from data_layer.model import StockUnit
+from data_layer.model import StockMainGroup
 
 
 def init_db():
--- data_layer/model/__init__.py
+++ data_layer/model/__init__.py
@@ -1,6 +1,7 @@
 from data_layer.model.crud_model import CRUD, Model, metadata
 
 from data_layer.model.definition.cashier import Cashier
-from data_layer.model.definition.stock_unit import StockUnit
 from data_layer.model.definition.vat import Vat
+from data_layer.model.definition.stock_unit import StockUnit
+from data_layer.model.definition.stock_main_group import StockMainGroup
 
