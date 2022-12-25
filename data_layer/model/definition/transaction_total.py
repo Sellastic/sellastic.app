0fe5cb198fc52cde285f44e2e59042154b621692
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2022-12-25 07:05:52 +0300

    TransactionTotal data model added.

---
+++ data_layer/model/definition/transaction_total.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, Integer, BigInteger, Boolean, String, DateTime, Float, ForeignKey, UUID
+from sqlalchemy.sql import func
+
+from data_layer.model.crud_model import Model
+from data_layer.model.crud_model import CRUD
+
+
+class TransactionTotal(Model, CRUD):
+    def __init__(self):
+        Model.__init__(self)
+        CRUD.__init__(self)
+
+    __tablename__ = "transaction_product"
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True, default=1)
+    fk_transaction_head_id = Column(BigInteger, ForeignKey("transaction_head.id"))
+    line_no = Column(Integer, nullable=False)
+    fk_department_main_group_id = Column(BigInteger, ForeignKey("department_main_group.id"), nullable=False)
+    total_department = Column(Float, nullable=False)
+    total_department_vat = Column(Float, nullable=False)
+    is_deleted = Column(Boolean, nullable=False, default=False)
+    delete_description = Column(String(1000), nullable=True)
+    fk_cashier_create_id = Column(BigInteger, ForeignKey("cashier.id"))
+    fk_cashier_update_id = Column(BigInteger, ForeignKey("cashier.id"))
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now())
+
+    def __repr__(self):
+        return f"<TransactionTotal(total_department='{self.total_department}', total_department_vat='{self.total_department_vat}')>"
